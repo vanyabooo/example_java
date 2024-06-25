7732338f610d85ceaf442f50567b993245dce994
vanyabooo <rise.vane@gmail.com>
2024-06-25 12:35:23 +0300

    hw 1 - done!

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -9,6 +9,6 @@ public class SearchTests {
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
 }
